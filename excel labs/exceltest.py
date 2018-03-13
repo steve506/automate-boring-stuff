6167edc79403955b3b40b8c255596896b36a744d
Steven Garcia <steven_garcia@mckinsey.com>
2018-03-13 14:31:03 -0600

    test openpyxl

--- excel labs/exceltest.py
+++ excel labs/exceltest.py
@@ -5,8 +5,16 @@ wb = openpyxl.load_workbook('example.xlsx')
 
 print(wb.sheetnames)
 
-sheet = wb['Sheet3']
-
-print(sheet)
+sheet = wb['Sheet1']
 
 print(type(sheet))
+
+# tuple(sheet['A1':'C3'])
+
+for rowOfCellObjects in sheet['A1':'C3']:
+    for cellObj in rowOfCellObjects:
+        print(str(cellObj.coordinate), str(cellObj.value))
+    print('--- END OF ROW ---')
+
+for cell in sheet['B']:
+    print(cell.value)
